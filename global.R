d3261cc6e7214e07869bb6be55fb488e122c2b32
Jacob Malcom <jmalcom@defenders.org>
2016-05-13 12:01:18 -0400

    No real changes.

--- global.R
+++ global.R
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-# system("touch restart.txt", intern = FALSE)
+system("touch restart.txt", intern = FALSE)
 
 #############################################################################
 # Load packages and source files
